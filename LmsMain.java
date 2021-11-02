8d21887191ac31635a51d1991b86f16dd6f322d5
manishankar <manishjaiswal152207@gmail.com>
2021-11-02 15:57:26 +0545

    minor project for college in sixth sem

---
+++ LmsMain.java
@@ -0,0 +1,18 @@
+import lms.*;
+class LmsMain
+{
+public static void main(String ab[])
+ {
+ CreateAcc ob=new CreateAcc();
+ //AddNewSts ob=new AddNewSts();
+ //AddNewBook ob=new AddNewBook();
+ //BookIssued ob=new BookIssued();
+ //BookReturn ob=new BookReturn();
+ //ChangePassword ob=new ChangePassword();
+ //ForgetPassword ob=new ForgetPassword();
+ //UpdateStsDetail ob=new UpdateStsDetail();
+ //UpdateNewBook ob=new UpdateNewBook();
+ //Login ob=new Login();
+ ob.setUpCreateAcc();
+ }
+}
